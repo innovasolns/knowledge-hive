1ac1dc79ffb4de8db7979ab403f725e08eb12a75
Jerome Raymond <admin@innovasolutionsllc.xyz>
2026-03-13 11:32:32 -0500

    Add Microsoft Clarity tracking script

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,6 +76,15 @@ export default function RootLayout({
             __html: `!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');twq('config','r9f0r');`,
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "vv740u58db");`,
+          }}
+        />
       </head>
       <body
         className={`${inter.variable} ${montserrat.variable} font-sans bg-navy-deep text-slate-200 antialiased`}
